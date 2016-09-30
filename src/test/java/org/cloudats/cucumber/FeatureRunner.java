4860d020b7f65c11ad9580afd2050873999258cd
Nguyen Thanh Hai (FSU1.TEST) <HaiNT21@fsoft.com.vn>
2016-09-30 14:18:19 +0700

    re-organize

--- src/test/java/org/cloudats/cucumber/FeatureRunner.java
+++ src/test/java/org/cloudats/cucumber/FeatureRunner.java
@@ -4,10 +4,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-		features = {"src/test/resources/Simple.feature"},
-		glue = {"org.cloudats.cucumber"},
+		features = {"src/test/resources/org/cloudats/cucumber/Simple.feature"},
+//		glue = {"org.cloudats.cucumber"},
 		plugin = {"pretty", "json:target/cucumber.json"}
 )
-public class DownloadFeature extends AbstractTestNGCucumberTests {
+public class FeatureRunner extends AbstractTestNGCucumberTests {
 
 }
